23b4d76d017d1a8807f12fc73552b90f48c806ad
Jacobo Gómez <jagomez2012@gmail.com>
2017-08-11 21:50:38 -0500

    Add hello world bot

---
+++ bancolucas.py
@@ -0,0 +1,28 @@
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+import os
+import todoDB
+import telegram
+import todoFilter
+
+def start(bot, update):
+    update.message.reply_text('Hi! Luckily, this bot works. Now, let\'s do stuff!')
+
+def main():
+    TOKEN = "382499494:AAEJrdhHmXy46VV-RrBv0xmkIJps09eJyD4"
+    updater = Updater(token=TOKEN)
+    dispatcher = updater.dispatcher
+    conv_handler = telegram.ext.ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+    dispatcher.add_handler(conv_handler)
+
+    PORT = int(os.environ.get('PORT', '5000'))
+    updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+    updater.bot.set_webhook("https://todolistbot.herokuapp.com/" + TOKEN)
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
