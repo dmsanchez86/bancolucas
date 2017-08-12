8e20921789ae5f6a63f600b64825010d7a546c1a
ar4z <oaraz02@gmail.com>
2017-08-12 18:35:22 -0500

    sss

--- bancoServices.py
+++ bancoServices.py
@@ -5,12 +5,14 @@ from bancoDB import DBHelper
 from telegram import ReplyKeyboardMarkup
 import bancoFilter
 
-OPERATIONS = 0
-ADD_BALANCE = 1
+ADD_BALANCE = 0
+ADD_BALANCE_NUMBER = 1
 def services(bot, update):
     reply_keyboard = [["Add fondos"]]
-    update.message.reply_text("¿Que deseas hacer?", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-    return OPERATIONS
+
+    update.message.reply_text("¿Que quieres hacer?", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return ADD_BALANCE
 
 def transfer(bot, update):
     pass
@@ -19,9 +21,15 @@ def cancel(bot, update):
     pass
 
 def add_balance(bot, update):
+    update.message.reply_text("Digita el total a añadir")
+    return ADD_BALANCE_NUMBER
+
+
+def add_balance_logic(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="hi")
 
-# def add_balance_logic(bot, update):
-#     bot.send_message(chat_id=update.message.chat_id, text="hi")
 
-add_balance_handler = CommandHandler("add", add_balance)
+add_balance_handler = ConversationHandler(entry_points=[MessageHandler(bancoFilter.filter_add_balance, add_balance)],
+                                          states={ADD_BALANCE: [MessageHandler(bancoFilter.filter_number, add_balance)],
+                                                  ADD_BALANCE_NUMBER: [MessageHandler(bancoFilter.filter_number, add_balance_logic)]},
+                                          fallbacks=[CommandHandler('cancel', cancel)])
